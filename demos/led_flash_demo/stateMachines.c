d4f500dcb2fe0a484f6ddafedcf8205c16408964
dgnajera <dgnajera2@miners.utep.edu>
2016-09-26 23:38:47 -0600

    Reworked interrupts

--- demos/led_flash_demo/stateMachines.c
+++ demos/led_flash_demo/stateMachines.c
@@ -5,9 +5,17 @@ static unsigned char count_red = 0;
 static unsigned char green_led_state = 0;
 static unsigned char count_green = 0;
 
-void led_control();
+void led_control(){
+  char ledFlags = 0;
+  if(green_led_state)
+    ledFlags |= BIT6;
+  if(red_led_state)
+    ledFlags |= BIT0;
+  P1OUT &= (0xff-(BIT6+BIT0)) | ledFlags; // clear bit for off leds
+  P1OUT |= ledFlags;        // set bit for on leds
+}
 
-void timerHandler(){
+__interrupt(TIMER0_A0_VECTOR) TIMER_A0(){
   count_red++;
   count_green++;
 
@@ -22,14 +30,4 @@ void timerHandler(){
     count_green = 0;
     led_control();
   }
-}
-
-void led_control(){
-  char ledFlags = 0;
-  if(green_led_state)
-    ledFlags |= BIT6;
-  if(red_led_state)
-    ledFlags |= BIT0;
-  P1OUT &= (0xff-(BIT6+BIT0)) | ledFlags; // clear bit for off leds
-  P1OUT |= ledFlags;        // set bit for on leds
 }
