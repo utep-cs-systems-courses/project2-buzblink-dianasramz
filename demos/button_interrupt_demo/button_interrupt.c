4b35fa8bf2b79a69015aeb05dd412da3eb328191
dgnajera <dgnajera2@miners.utep.edu>
2016-09-26 20:42:58 -0600

    Reworked demo to utilize buttons on boosterpack board. Also changed behavior interrupt

---
+++ demos/button_interrupt_demo/button_interrupt.c
@@ -0,0 +1,20 @@
+#include <msp430.h>
+#include "buttons.h"
+
+void led_control(){
+  char ledFlags = 0; /* by default, no LEDs on */
+
+  if(!((P2IN & (~P2IN | S1)) == S1)) // S1 pressed
+    ledFlags = BIT0;    // red led
+
+  if(!((P2IN & (~P2IN | S2)) == S2)) // S2 pressed
+    P1OUT = BIT6;   // green led
+
+  P1OUT &= (0xff-(BIT0+BIT6)) | ledFlags; // clear bits for off leds
+  P1OUT |= ledFlags;         // set bits for on leds
+}
+
+/* Switches on P2 (S1 and S2) */
+__interrupt(PORT2_VECTOR) Port_2(){
+  led_control();
+}
---
+++ demos/button_interrupt_demo/buttons.h
@@ -0,0 +1,3 @@
+/* P2 bits for buttons */
+# define S1 BIT0
+# define S2 BIT1
