61423473e0e8a609b912d14ffda6be00df922d40
dgnajera <dgnajera2@miners.utep.edu>
2016-09-28 22:19:49 -0600

    State machines now interact

--- demos/led_flash_demo/stateMachines.c
+++ demos/led_flash_demo/stateMachines.c
@@ -6,25 +6,25 @@ static unsigned char green_led_state = 0;
 static unsigned char count_green = 0;
 
 void led_control(){
+  count_red++;
+  if(count_red >= 4){
+    red_led_state = 1 - red_led_state;
+    count_red = 0;
+  }
+
   char ledFlags = 0;
   if(green_led_state)
     ledFlags |= BIT6;
+
   if(red_led_state)
     ledFlags |= BIT0;
+  
   P1OUT &= (0xff-(BIT6+BIT0)) | ledFlags; // clear bit for off leds
   P1OUT |= ledFlags;        // set bit for on leds
 }
 
 __interrupt(TIMER0_A0_VECTOR) TIMER_A0(){
-  count_red++;
   count_green++;
-
-  if(count_red >= 100){
-    red_led_state = 1 - red_led_state;
-    count_red = 0;
-    led_control();
-  } 
-
   if(count_green >= 25){
     green_led_state = 1 - green_led_state;
     count_green = 0;
