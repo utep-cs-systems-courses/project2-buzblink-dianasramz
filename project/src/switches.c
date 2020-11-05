7d02e1edc99a69669c65dd6505f2b177d22d94c7
dianasramz <dianaranz64@gmail.com>
2020-11-04 22:06:23 -0700

    modified ToyMain, Makefile, p2_interrupt, and the switches

--- project/src/switches.c
+++ project/src/switches.c
@@ -1,26 +1,29 @@
 #include <msp430.h>
 #include "switches.h"
 #include "led.h"
+#include "libTimer.h"
+#include "buzzer.h"
+#include "stateMachine.h"
 
-char switch_state_down, switch_state_changed; /* effectively boolean */
+char switch1_state_down, switch2_state_down, switch3_state_down, switch4_state_down, switch_state_changed; /* effectively boolean */
 
 static char 
 switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
+  P2IES |= (p2val & SWITCHES);	/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);	/* if switch down, sense up */
+  return p2val;
 }
 
 void 
 switch_init()			/* setup switch */
 {  
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE |= SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
+  P2REN |= SWITCHES;		/* enables resistors for switches */
+  P2IE |= SWITCHES;		/* enable interrupts from switches */
+  P2OUT |= SWITCHES;		/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;		/* set switches' bits for input */
   switch_update_interrupt_sense();
   led_update();
 }
@@ -28,8 +31,13 @@ switch_init()			/* setup switch */
 void
 switch_interrupt_handler()
 {
-  char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
-  switch_state_changed = 1;
+  char p2val = switch_update_interrupt_sense();
+  switch1_state_down = (p2val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  switch2_state_down = (p2val & SW2) ? 0 : 1; /* 0 when SW2 is up */
+  switch3_state_down = (p2val & SW3) ? 0 : 1; /* 0 when SW3 is up */
+  switch4_state_down = (p2val & SW4) ? 0 : 1; /* 0 when SW4 is up */
+  if(switch1_state_down){
+    button_state = 1;
+  }
   led_update();
 }
